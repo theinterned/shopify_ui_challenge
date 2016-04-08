54a04ee80a6ca5c766ea7961542f4af52b29a669
Ned Schwartz <ned@theinterned.net>
2016-04-08 14:48:23 -0400

    Contact form now excepts values

--- js/components/views/ContactForm.react.js
+++ js/components/views/ContactForm.react.js
@@ -16,14 +16,25 @@ class ContactForm extends Component {
     }
   }
   render() {
+    const {name, phone_number, email_address} = this.props;
     return (
       <fieldset>
-        <Control ref="name" attr="name" label="Name" type="text" required="true" />
-        <Control ref="phone_number" attr="phone_number" label="Phone" type="tel" />
-        <Control ref="email_address" attr="email_address" label="Email" type="email" />
+        <Control ref="name" attr="name" label="Name" type="text" required="true" value={name} />
+        <Control ref="phone_number" attr="phone_number" label="Phone" type="tel" value={phone_number} />
+        <Control ref="email_address" attr="email_address" label="Email" type="email" value={email_address} />
       </fieldset>
     );
   }
+}
+ContactForm.defaultProps = {
+  name: "",
+  phone_number: "",
+  email_address: ""
+}
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  phone_number: PropTypes.string,
+  email_address: PropTypes.string
 }
 
 // Wrap the component to inject dispatch and state into it
